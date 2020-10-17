f7beef79c8789fa7ad38ba6efda9880c7aa4cb9b
Sun Yucheng <53768585+Cheng20010201@users.noreply.github.com>
2020-10-17 11:02:36 +0800

    Fid local connection issue & add indentation
    Fixed issue #3

--- index.js
+++ index.js
@@ -5,31 +5,32 @@ const PORT = process.env.PORT || 5000;
 // postgre data base connection logic
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+	// the string below can be substituted with your own postgre information, to get it work locally
+    	connectionString: process.env.DATABASE_URL || 'postgresql://<your postgre local user>:<your password for the user>@localhost:<your local port>/<your test db name>',
+    	ssl: process.env.DATABASE_URL ? true : false
+})
 
+// main logic
 express()
 	.use(express.static(path.join(__dirname, 'public')))
 	.set('views', path.join(__dirname, 'views'))
 	.set('view engine', 'ejs')
-	// .get('/', (req, res) => res.render('pages/index'))
-	.get('/', (req, res) => res.send('Hello, this is CS2102 group 56! Hello, this is CS2102 group 56!'))
+	// get index page
+	.get('/', (req, res) => res.render('pages/index'))
+	// get hello message
+	.get('/hello', (req, res) => res.send('Hello, this is CS2102 group 56! Hello, this is CS2102 group 56!'))
+	// get all people; DB connection test
 	.get('/people', async (req, res) => {
-    	  try {
-      	    const client = await pool.connect();
-	    // need to create the table using remote databse connection, 
-	    // or an error will be thrown
-      	    const result = await client.query('SELECT * FROM collaborators;');
-      	    const results = { 'results': (result) ? result.rows : null};
-      	    // render the result using pre-defined pages/db rendering
-	    res.render('pages/db', results );
-      	    client.release();
-    	  } catch (err) {
-      	    console.error(err);
-      	    res.send("Error " + err);
-    	  }
+    		try {
+			// connection is remote
+      	  		const client = await pool.connect();
+      	  		const result = await client.query('SELECT * FROM collaborators;');
+      	    		const results = { 'results': (result) ? result.rows : null};
+	    		res.render('pages/db', results );
+      	    		client.release();
+    	  	} catch (err) {
+      	    		console.error(err);
+      	    		res.send("Error " + err);
+    	  	}
   	})
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
