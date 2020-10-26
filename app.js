1bc9d79201f09f3a2ca4eebd43334a7a8b966625
zhaohuanqdcn <61936274+zhaohuanqdcn@users.noreply.github.com>
2020-10-26 17:38:23 +0800

    Application Setup

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const routes = require('./routes');
+var app = express();
+
+// this is the port for the whole app, not db
+const PORT = process.env.PORT || 5000; 
+
+// prepare for pool connection
+// using pool can facilitate multiple concurrent queries
+const { Pool } = require('pg');
+global.pool = new Pool({	
+	// For remote db connection:
+    	/*connectionString: // the url in remote db credential doc
+    	ssl: {
+    		rejectUnauthorized: false
+    	}*/
+    // For local db connection:
+    connectionString: 'postgresql://postgres:abc123456@localhost:5432/project', // your local db url
+    ssl: false
+});
+
+// main logic
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', routes.home);
+	// hello message demo
+app.get('/hello', routes.hello);
+	// about us
+app.get('/people', routes.aboutus);
+	// login page
+app.get('/login', routes.login);
+app.get('/signup', routes.signup);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const HELLO = 'Hello, this is CS2102 group 56!'
+const GET_COLLABORATORS = 'SELECT * FROM collaborators;';
+
+// homepage
+exports.home = (req, res) => res.render('pages/index');
+// hello message
+exports.hello = (req, res) => res.send(HELLO);
+// about us
+exports.aboutus = async (req, res) => {
+		try {
+			const client = await global.pool.connect();
+			const result = await client.query(GET_COLLABORATORS);
+			const results = { 'results': (result) ? result.rows : null};
+			res.render('pages/collaborators', results );
+			client.release();
+		} catch (err) {
+			console.error(err);
+			res.send("Error " + err);
+		}
+	};
+// login page
+exports.login = (req, res) => res.render('pages/login');
+// signup page
+exports.signup = (req, res) => res.render('pages/signup');
+
+
