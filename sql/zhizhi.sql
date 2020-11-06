6ae95835657b81cc0d00b2c8c7111c160bb14c29
zhizhizhizhizhi <zhizhi.xu@yahoo.com>
2020-11-06 20:03:22 +0800

    updated salary function again

--- sql/zhizhi.sql
+++ sql/zhizhi.sql
@@ -67,7 +67,7 @@ BEGIN
         WHERE b.ctemail = mail and b.success = true and EXTRACT (MONTH FROM b.endDate) = month and EXTRACT (YEAR FROM b.endDate) = year;
     ELSIF ttype = 'full time' THEN
         SELECT  CASE WHEN SUM(bonus.price) IS NULL then 3000
-                ELSE SUM(bonus.price) + 3000
+                ELSE SUM(bonus.price * 0.8) + 3000
                 END into salary
         FROM (SELECT DISTINCT c.ctemail, c.at, c.pet_owner, c.pet_name, b.price
         FROM caretaker_cares_at as c JOIN bids as b on b.ctemail = c.ctemail and b.success and b.poemail = c.pet_owner and b.name = c.pet_name and c.at BETWEEN b.startDate AND b.endDate
